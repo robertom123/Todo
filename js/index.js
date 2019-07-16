869a79a77fd238b54b20f3e55eb7b8408a418a9f
robertom123 <romarto0521@gmail.com>
2019-07-16 13:10:23 -0700

    update

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-
+//creating elements
 var list = document.createElement("li");
 var span = document.createElement("span");
 var btn = document.createElement("button");
-var main = document.getElementById("main"); // this the ul 
+var main = document.getElementById("main"); // this is the ul tag 
 var info = document.getElementById("info");
 
-
+//adding the event listeners 
 document.getElementById("btn1").addEventListener('click', add, false);
 document.getElementById("filter").addEventListener('keyup', filterlist, false);
 
-
+//creating add function
 function add(){
-    
+    //value from input 
     var msg = document.getElementById("add").value;
+    //checks if input value is empty to display span text
     if(msg === ''){
         info.style.display = 'block';
     }
+
+    //creates elements if input value is not empty 
     else {
         info.style.display = 'none';
         var main = document.getElementById("main");
@@ -32,7 +35,6 @@ function add(){
 
         // This className for span
         // This classname for button  
-        // take off the li stuff 
         // add checkbox with line through 
         // add custom buttons 
 
@@ -57,20 +59,20 @@ function add(){
         list.appendChild(btn);
 
         main.appendChild(list);
+
        document.getElementById('add').value = '';
     }
 
 }
 
 
-//potential bug 
 function filterlist(e){
-    var text = e.target.value.toLowerCase(); //<<-  THIS gets the value and changes it to lowercase 
+    var text = e.target.value.toLowerCase(); //<<-gets value and changes it to lowercase 
     var items = main.getElementsByTagName('li'); //<-- gets the li in ul 
 
 
     Array.from(items).forEach(function(item){ //<<-- converts the list to an array then loop the items 
-        var itemsName = item.textContent; //<-- gets the itmes on the list 
+        var itemsName = item.textContent; //<-- gets the item on the list 
         if(itemsName.toLowerCase().indexOf(text) != -1){ // <-- checks if the itmes is in the list 
             item.style.display = "block"; //<-- display if able to see 
         }
